a156f8a7dc771010f346d77d1d9281bf5007bf05
Prashant <91491749+prashant18vit@users.noreply.github.com>
2022-07-27 14:20:31 -0400

    timeline cchange

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Worked as a intern in ISOBEX as a programmer analyst for developing wbscraping app and Quiz app', },
+  { year: 2021, text: 'Worked on a student Dashboard', },
+  { year: 2022, text: 'Shared my projects with the world', },
+  { year: 2022, text: ' currently Working at cognizant ', },
 ];
